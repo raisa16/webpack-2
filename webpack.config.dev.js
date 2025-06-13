ec58db6d6aa3dc727f077597560863d86a13a242
Raisa <git>
2025-06-13 09:35:08 -0700

    update settings

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     // mode: 'production', // LE INDICO EL MODO EXPLICITAMENTE
@@ -17,7 +17,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development',
-    watch: true,
+    devtool: 'source-map',
     resolve: {
         extensions: ['.js'], // LOS ARCHIVOS QUE WEBPACK VA A LEER
     
@@ -84,5 +84,12 @@ module.exports = {
       ]
     }),
     new Dotenv(),
+     new BundleAnalyzerPlugin(),
     ],
+    devServer: {
+  static: path.join(__dirname, 'dist'),
+  compress: true,
+  historyApiFallback: true,
+  port: 3000
+},
 }
